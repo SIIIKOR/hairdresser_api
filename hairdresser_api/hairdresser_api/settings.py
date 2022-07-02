2915093238a7dfc7c02e0a5710a47066a2e3c904
SIIIKOR <siiikor.sikorski@gmail.com>
2022-07-02 02:16:45 +0200

    Created psql database for the project

--- hairdresser_api/hairdresser_api/settings.py
+++ hairdresser_api/hairdresser_api/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # installed apps
+    'rest_framework',
+
+    # my apps
+    'hairdressers',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'hairdresser_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hairdresserapi',
+        'USER': 'mateusz',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
