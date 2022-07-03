789117108db39f096a470b2ac033eb3b3cc3db62
SIIIKOR <siiikor.sikorski@gmail.com>
2022-07-03 21:12:19 +0200

    Created models

--- hairdresser_api/hairdressers/models.py
+++ hairdresser_api/hairdressers/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=60)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    estimated_time = models.DurationField()
+
+
+class Haidresser(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    email = models.EmailField()
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.PROTECT)
+    haidresser = models.ForeignKey(Haidresser, on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, on_delete=models.PROTECT)
+    # end time will be calculated via service estimated time.
+    start_time = models.DateTimeField()
